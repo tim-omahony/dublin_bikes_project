2bffff8a0c0c12cfafb4d801d1bbe3f78be79643
Tim <timothyomahony@gmail.com>
2021-03-18 19:18:08 +0000

    use db_details instead of hardcoding

--- config/db_session.py
+++ config/db_session.py
@@ -1,13 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import connectors
-import pymysql
+from db_details import *
 
-USER = "root"
-PASSWORD = "Ylu3shin123!"
-URI = "127.0.0.1"
-PORT = "3306"
-DB = "dbbikes"
 engine = create_engine("mysql+pymysql://{}:{}@{}:{}/{}".format(USER, PASSWORD, URI, PORT, DB), echo=True)
 
 # create a configured "Session" class
